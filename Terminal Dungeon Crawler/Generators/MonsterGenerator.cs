e80fc3c7d1c0a6b375f1a8fc4ae8be9203e55e62
Ivan Cavalheiro <ivan.cavalheiro@hogarth.com>
2023-05-07 12:45:47 -0300

    adding a base for the generators

---
+++ Terminal Dungeon Crawler/Generators/MonsterGenerator.cs
@@ -0,0 +1,7 @@
+namespace ProjetosFacul.TerminalDungeonCrawler.Generators;
+
+public static class MonsterGenerator
+{
+    private const string ProfessionsCsvPath = "./monster_professions.csv";
+    private const string RacesCsvPath = "./monster_races.csv";
+}
