db66cc82da9fa729fc09b791616996434eea70e7
Ivan Cavalheiro <ivan.cavalheiro@hogarth.com>
2023-05-07 13:44:45 -0300

    refactored the data files and added a monster generator

--- Terminal Dungeon Crawler/Generators/MonsterGenerator.cs
+++ Terminal Dungeon Crawler/Generators/MonsterGenerator.cs
@@ -1,7 +1,75 @@
+using System.IO;
 namespace ProjetosFacul.TerminalDungeonCrawler.Generators;
 
 public static class MonsterGenerator
 {
-    private const string ProfessionsCsvPath = "./monster_professions.csv";
-    private const string RacesCsvPath = "./monster_races.csv";
+    private const string ProfessionsCsvPath = "./Data/monster_professions.csv";
+    private const string RacesCsvPath = "./Data/monster_races.csv";
+    private static Random Random = new();
+
+
+    private static string GetRace()
+    {
+        using (var file = File.OpenText(RacesCsvPath))
+        {
+            var content = file.ReadToEnd();
+            var lines = content.ReplaceLineEndings("\n").Split("\n");
+            return lines[Random.Next(0, lines.Length)];
+        }
+    }
+
+    private static string GetProfession()
+    {
+        using (var file = File.OpenText(ProfessionsCsvPath))
+        {
+            var content = file.ReadToEnd();
+            var lines = content.ReplaceLineEndings("\n").Split("\n");
+            return lines[Random.Next(0, lines.Length)];
+        }
+    }
+
+    private static int GetAttack(int difficultyLevel)
+    {
+        const int baseAttack = 10;
+        const int attackPerLevel = 3;
+
+        var attack = baseAttack + (int)((attackPerLevel * difficultyLevel) * ((Random.NextDouble() * 0.6f) + 1.1f));
+
+        return attack;
+    }
+
+    private static int GetArmor(int difficultyLevel)
+    {
+        const int baseArmor = 0;
+        const int armorPerLevel = 1;
+
+        var armor = baseArmor + (int)((armorPerLevel * difficultyLevel) * ((Random.NextDouble() * 0.6f) + 1.1f));
+
+        if (armor > 15)
+            return 15;
+
+        return armor;
+    }
+
+    private static int GetLife(int difficultyLevel)
+    {
+        const int baseLife = 150;
+        const int lifePerLevel = 20;
+
+        var life = baseLife + (int)((lifePerLevel * difficultyLevel) * ((Random.NextDouble() * 3f) + 1f));
+
+        return life;
+    }
+
+    public static Monster GenerateOne(int difficultyLevel)
+    {
+        return new()
+        {
+            Profession = GetProfession(),
+            Race = GetRace(),
+            Attack = GetAttack(difficultyLevel),
+            Life = GetLife(difficultyLevel),
+            Armor = GetArmor(difficultyLevel)
+        };
+    }
 }
--- Terminal Dungeon Crawler/Monster.cs
+++ Terminal Dungeon Crawler/Monster.cs
@@ -2,10 +2,10 @@ namespace ProjetosFacul.TerminalDungeonCrawler;
 
 public class Monster
 {
-    public string Name { get; set; } = "Secret";
     public int Attack { get; set; } = 10;
     public int Life { get; set; } = 100;
     public int Armor { get; set; } = 1;
-    public string Description { get; set; } = "No description available";
+    public string Profession { get; set; } = "Undefined";
     public string Race { get; set; } = "Undefined";
+    public bool PlayerAware { get; set; } = false;
 }
